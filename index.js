fa0674b48bd94a690458d7037428b4f20e10c6a3
alyxailes <alyssadoingthehustle@gmail.com>
2022-12-08 18:32:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const itemList = [];
+let index = 1;
+let command = '';
+
+while (command !== "quit") {
+    command = toLowerCase(prompt("COMMAND : What do you want to do?"));
+
+    if (command === "new") {
+        let addItem = prompt("NEW : What do you want to add into your ToDo?");
+        itemList.push(addItem);
+        index = 1;
+        console.clear();
+        console.log("ToDo List : ")
+        for (let item in itemList) {
+            console.log(`"${index} : ${itemList[index - 1]}"`);
+            index++;
+        }
+        console.log("---------------")
+    } else if (command === "list") {
+        console.clear();
+        index = 1;
+        console.log("ToDo List : ")
+        for (let item in itemList) {
+            console.log(`"${index} : ${itemList[index - 1]}"`);
+            index++;
+        }
+        console.log("---------------")
+    } else if (command === "delete") {
+        index = 1;
+        let indexDelete = prompt("DELETE : What list do you want to delete?");
+        itemList.splice(indexDelete - 1, 1);
+        console.clear();
+        index = 1;
+        console.log("ToDo List : ")
+        for (let item in itemList) {
+            console.log(`"${index} : ${itemList[index - 1]}"`);
+            index++;
+        }
+        console.log("---------------")
+    }
+}
+console.log("Okay! You quit the app!")
