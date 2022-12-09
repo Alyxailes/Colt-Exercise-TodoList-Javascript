e307c6cae921ef4daf6e4214c7bbab9af557e3a2
alyxailes <alyssadoingthehustle@gmail.com>
2022-12-09 17:03:06 +0800

    made function for showing ToDo List and optimized the logic for asking an index

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
 const itemList = [];
-let index = 1;
 let command = prompt("COMMAND : What do you want to do?").toLowerCase();
 
 while (command !== "quit") {
     if (command === "new") {
         let addItem = prompt("NEW : What do you want to add into your ToDo?");
         itemList.push(addItem);
-        index = 1;
-        console.clear();
-        console.log("ToDo List : ")
-        for (let item in itemList) {
-            console.log(`"${index} : ${itemList[index - 1]}"`);
-            index++;
-        }
-        console.log("---------------")
+        showTodo();
     } else if (command === "list") {
-        console.clear();
-        index = 1;
-        console.log("ToDo List : ")
-        for (let item in itemList) {
-            console.log(`"${index} : ${itemList[index - 1]}"`);
-            index++;
-        }
-        console.log("---------------")
+        showTodo();
     } else if (command === "delete") {
         index = 1;
         let indexDelete = prompt("DELETE : What list do you want to delete?");
-        if (indexDelete !== NaN) {
+        if (indexDelete <= itemList.length && indexDelete >= 1 && isNaN(indexDelete) == false) {
             itemList.splice(indexDelete - 1, 1);
-            console.clear();
-            index = 1;
-            console.log("ToDo List : ")
-            for (let item in itemList) {
-                console.log(`"${index} : ${itemList[index - 1]}"`);
-                index++;
-            }
-            console.log("---------------")
-        } else {
-            let indexDelete = prompt("DELETE : You entered an invalid number! What list do you want to delete?");
+            showTodo();
         }
     }
     command = prompt("COMMAND : What do you want to do?").toLowerCase();
 }
 console.log("Okay! You quit the app!")
+
+function showTodo() {
+    console.clear();
+    console.log("ToDo List : ")
+    for (let item in itemList) {
+        console.log(`"${parseInt(item) + 1} : ${itemList[item]}"`);
+    }
+    console.log("---------------")
+}
